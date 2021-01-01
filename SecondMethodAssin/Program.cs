8f5a9a12af984dacbfd424adaa6550064003ecca
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-12-31 17:01:13 -0700

    Add project files.

---
+++ SecondMethodAssin/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SecondMethodAssin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            twoMeths math = new twoMeths();
+            int userNum;
+            Console.WriteLine("Input two numbers, one at a time. You don't need to enter anything for the second number");
+            userNum = Convert.ToInt32(Console.ReadLine());
+            math.OptionMath(userNum);
+            Console.WriteLine(math.OptionMath(userNum));
+        }
+    }
+}
---
+++ SecondMethodAssin/twoMeths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecondMethodAssin
+{
+    public class twoMeths
+    {
+        public int OptionMath(int num1, int num2 = 10)
+        {
+            int result;
+            result = num1 + 11;
+            return result;
+        }
+    }
+}
